4080bcfbc19d79982db9956d183315dba46088fb
fsoest <felix@soest-net.de>
2021-07-19 22:15:29 +0200

    Code for lattice should work now

--- lattice.py
+++ lattice.py
@@ -16,8 +16,7 @@ def random_snake(g, d):
     # First step is random
     n_0 = list(g[initial_node])
     plan_index = np.random.choice(np.arange(len(n_0)))
-    plan.append(list(g)[plan_index])
-
+    plan.append(n_0[plan_index])
     route.append(initial_node)
 
     for _ in range(10):
@@ -26,22 +25,32 @@ def random_snake(g, d):
             neighbours = list(g[plan[-1]])
         except:
             neighbours = list(g[initial_node])
-        # Check suitability of neighbours
+            # print(plan)
+            # print(route)
+        # # Check suitability of neighbours
+        suitables = []
+        print(_)
+        print('Plan', plan)
+        print('route', route)
+        print('neighbours', neighbours)
         for n in neighbours:
-            if spl[route[-1]][n] != spl[route[-1]][plan[-1]] + spl[plan[-1]][n] or spl[route[-1]][n] > d:
-                neighbours.remove(n)
+            if spl[route[-1]][n] == spl[route[-1]][plan[-1]] + spl[plan[-1]][n] and spl[route[-1]][n] <= d:
+                suitables.append(n)
+        print('suitables', suitables)
         # Choose random neighbour from list of suitable neighbours if list nonempty
-        if len(neighbours) > 0:
-            n_index = np.random.choice(np.arange(len(neighbours)))
-            plan.append(neighbours[n_index])
+        if len(suitables) > 0:
+            n_index = np.random.choice(np.arange(len(suitables)))
+            plan.append(suitables[n_index])
         else:
             route.append(plan[0])
             plan.remove(plan[0])
-        print(route)
-        print(plan)
+
+    # print(route)
+    # print(plan)
 
 
 if __name__ == '__main__':
+    np.random.seed(45)
     lattice_size = 5
     square_lattice = nx.grid_graph((lattice_size, lattice_size), periodic=True)
 
