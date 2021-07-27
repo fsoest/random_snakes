1f70d5d8ab1ace7c79ad97a101424b1044418211
jlindermeir <jakob@skasa.de>
2021-07-27 14:25:46 +0200

    Code style changes

--- save.py
+++ save.py
@@ -1,10 +1,15 @@
+import pathlib
 import pickle
 
 
+OBJ_DIR = pathlib.Path(__file__).parent / 'obj'
+
+
 def save_obj(obj, name):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
+    with open(OBJ_DIR / f'{name.pkl}', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+
 def load_obj(name):
-    with open('obj/' + name + '.pkl', 'rb') as f:
+    with open(OBJ_DIR / f'{name.pkl}', 'rb') as f:
         return pickle.load(f)
