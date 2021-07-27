436b647a92b0d298fa2a769505acca5f039c5ea5
jlindermeir <jakob@skasa.de>
2021-07-27 14:30:47 +0200

    Fix a bug

--- save.py
+++ save.py
@@ -6,10 +6,10 @@ OBJ_DIR = pathlib.Path(__file__).parent / 'obj'
 
 
 def save_obj(obj, name):
-    with open(OBJ_DIR / f'{name.pkl}', 'wb') as f:
+    with open(OBJ_DIR / f'{name}.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
 def load_obj(name):
-    with open(OBJ_DIR / f'{name.pkl}', 'rb') as f:
+    with open(OBJ_DIR / f'{name}.pkl', 'rb') as f:
         return pickle.load(f)
